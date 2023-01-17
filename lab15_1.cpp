7bac7b8dce6e880be55acce093e6c6d04e36d8ee
PluemW <jaroenwachirasak@gmail.com>
2023-01-17 17:04:49 +0700

    Filled

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	T key;
+	for(int i=1; i<N; i++){
+		int j = i-1;
+		while(j>=0 && d[j+1]>d[j]){
+			key = d[j+1];
+			d[j+1] = d[j];
+			d[j] = key;
+			j--;
+		}
+		cout <<"Pass "<< i << ":" ;
+		for(int k = 0; k < 10; k++) cout << d[k] << " ";
+		cout << "\n";
+	}
 }
 
 int main(){
